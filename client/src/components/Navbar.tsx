cf2f6b6f8147ad9078c5ec6a82d3d0b386597b61
babinicz <babinicz@onet.pl>
2025-06-02 16:40:34 +0200

    zm

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -130,6 +130,9 @@ export default function Navbar() {
   };
 
   const navLinkHref = (pageKey: PageKey): string => {
+    if (pageKey === PAGE_KEYS.HOME) {
+      return "";
+    }
     return getInternalRoutePath(pageKey, language);
   };
 
